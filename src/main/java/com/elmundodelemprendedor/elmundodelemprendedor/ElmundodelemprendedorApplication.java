3e997a445854f94ebff4abe75b3cce96bc9af2c3
washingtondnavarro <washington.d.navarro@gmail.com>
2018-03-01 12:44:07 -0600

    Adding the Landing Page

---
+++ src/main/java/com/elmundodelemprendedor/elmundodelemprendedor/ElmundodelemprendedorApplication.java
@@ -0,0 +1,12 @@
+package com.elmundodelemprendedor.elmundodelemprendedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmundodelemprendedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElmundodelemprendedorApplication.class, args);
+	}
+}
